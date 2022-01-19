664df60a3b04c0b5258c018e4d5befd7590358dc
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-01-19 17:57:00 -0500

    incorporate react-rnd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,35 @@
+import { Rnd } from 'react-rnd';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+
+<Rnd
+default={{
+x: 0,
+y: 0,
+width: 320,
+height: 200,
+}}
+>
+Conversation 1
+</Rnd>
+
+<Rnd
+default={{
+x: 100,
+y: 100,
+width: 320,
+height: 200,
+}}
+>
+Conversation 2
+</Rnd>
+
+</div>
+);
 }
 
 export default App;
